c74840d5ece334b548fec6c7442e42bc6e043c53
losinyen <nachuchphilip@gmail.com>
2024-06-19 08:06:03 +0300

    4 passed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+
+    fib_sequence = [0, 1]
+    for i in range(2, length):
+        next_num = fib_sequence[i-1] + fib_sequence[i-2]
+        fib_sequence.append(next_num)
+
+    print(fib_sequence)
